f2f8831b9ffaf954d04594c7382729b25d48fa06
Ivy-Wanjiku <wanjiruwanjikuivy@gmail.com>
2024-03-27 22:16:14 +0300

    changes

--- arrays.js
+++ arrays.js
@@ -4,20 +4,26 @@ let arr2=[true,"green","where",12,56];
 console.log(arr2[arr2.length-1]);
 stringMyPets=["Cow","Bird","Snake","Dog"];
 console.log(stringMyPets.toString());
+console.log(stringMyPets.join());
 const arr3=[-5,9,5,3,2,-3,6,8,4,1];
 arr3.sort()
 console.log(arr3);
-var arr=["apples","mango","apple","orange","mango","mango"];
+let arr=["apples","mango","apples","orange","mango","mango"];
+let arrDuplicate=[]
+let arrOriginal=[]
+arr.forEach (y =>{
+	if(!arrOriginal.includes(y)){
+		arrOriginal.push(y);
 
-function removeDuplicates(arr) {
-	var unique = arr.reduce(function (acc, curr) {
-		if (!acc.includes(curr))
-			acc.push(curr);
-		return acc;
-	}, []);
-	return unique;
-}
-console.log(removeDuplicates(arr));
+	}
+	else{
+		arrDuplicate.push(y);
+	}
+});
+
+console.log({arrOriginal});
+console.log({arrDuplicate});
+
 
 let arr5=["the","way","x",4];
 let substring="way"
